7de2fc35c1d78b9b6d020a0f8f4466bfa0958c57
Rogério Toledo <rotoledojr@gmail.com>
2021-02-01 09:46:31 -0300

    Landing page setup

---
+++ Fitness/Fitness/FitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessApp.swift
+//  Fitness
+//
+//  Created by Rogério Toledo on 01/02/21.
+//
+
+import SwiftUI
+
+@main
+struct FitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LandingView()
+        }
+    }
+}
---
+++ Fitness/Fitness/View/LandingView.swift
@@ -0,0 +1,59 @@
+//
+//  LandingView.swift
+//  Fitness
+//
+//  Created by Rogério Toledo on 01/02/21.
+//
+
+import SwiftUI
+
+struct LandingView: View {
+    @State private var isActive: Bool = false
+    
+    var body: some View {
+        NavigationView {
+            GeometryReader { geo in
+                VStack {
+                    Spacer()
+                        .frame(height: geo.size.height * 0.18)
+                    Text("Increment")
+                        .font(.system(size: 64, weight: .medium))
+                        .foregroundColor(.white)
+                    Spacer()
+                    
+                    NavigationLink(
+                        destination: Text("navigated"), isActive: $isActive) {
+                        Button(action: { isActive = true }, label: {
+                            HStack(spacing: 15){
+                                Spacer()
+                                
+                                Image(systemName: "plus.circle")
+                                    .font(.system(size: 24, weight: .semibold))
+                                    .foregroundColor(.white)
+                                
+                                Text("Create a Challenge")
+                                    .font(.system(size: 24, weight: .semibold))
+                                    .foregroundColor(.white)
+                                
+                                Spacer()
+                            }
+                        })
+                        .padding(.horizontal, 15)
+                        .buttonStyle(PrimaryButtonStyle(fillColor: .darkPrimaryButton))
+                    }
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(
+                    Image("pullup")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .overlay(Color.black.opacity(0.4))
+                        .frame(width: geo.size.width)
+                        .edgesIgnoringSafeArea(.all)
+                )
+            }
+        }
+        .accentColor(.primary)
+    }
+}
+
